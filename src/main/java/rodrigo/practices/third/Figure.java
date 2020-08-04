0b44888823b6b95fd1c60f723ae1438801afd243
roht0515 <rodrigo051599@gmail.com>
2020-08-04 06:41:57 -0400

    figure class

---
+++ src/main/java/rodrigo/practices/third/Figure.java
@@ -0,0 +1,29 @@
+abstract class Figure {
+    protected final static int WHITE = 0;
+    protected final static int BLACK = 1;
+
+    protected int color;
+
+    /** Identifier name of a figure */
+    protected String identifier;
+
+    /** Every figure has a color */
+    Figure(int color) {
+        this.color = color;
+        identifier = "";
+    }
+
+    /** Get the figure color */
+    public int getColor() {
+        return this.color;
+    }
+
+    /** For testing purposes */
+    public String whoAmI() {
+        if (color == Figure.WHITE) {
+            return "WHITE";
+        } else {
+            return "BLACK";
+        }
+    }
+}
