9a1d612053b6862134dad9397aeba69c2dd312aa
roht0515 <rodrigo051599@gmail.com>
2020-08-04 06:43:02 -0400

    queen class

---
+++ src/main/java/rodrigo/practices/third/Queen.java
@@ -0,0 +1,18 @@
+public class Queen extends Figure {
+    public Queen(int color) {
+        super(color);
+    }
+
+    public String whoAmI() {
+        String type = super.whoAmI();
+        return type + " " + "Queen";
+    }
+
+    /**
+     * Not needed for this exercise.
+     */
+    public boolean move(Position origin, Position destination) {
+        // This is not correct, but we will not need it for this exercise
+        return true;
+    }
+}
