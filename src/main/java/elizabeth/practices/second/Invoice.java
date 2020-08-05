194e2309cc756fbe17b3ba66c9488c56ee3a5857
eli7890 <eliz.bf.4422@gmail.com>
2020-08-05 06:47:48 -0400

    Solved error of check style class invoice

--- src/main/java/elizabeth/practices/second/Invoice.java
+++ src/main/java/elizabeth/practices/second/Invoice.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Invoice {
-    private List<Product> products;
+    private final List<Product> products;
 
     public Invoice(List<Product> tempProducts) {
         //products = tempProducts;
@@ -13,6 +13,11 @@ public class Invoice {
             products.add(product);
         }
     }
+
+    /**
+     *
+     * @return int totalCost
+     */
     public int getTotalCost() {
         int totalCost = 0;
         for (Product product: products) {
@@ -22,32 +27,39 @@ public class Invoice {
         return totalCost;
     }
 
+    /**
+     *
+     * @return String getDetail
+     */
     public String getDetail() {
         String header = "Cant\tProduct\t\tPrice\tTotal\n";
         String headline = "-------------------------------\n";
         String detail = "";
         for (Product product: products) {
-            detail = detail + product.getQuantity()+ "\t\t";
-            detail = detail + product.getName()+ "\t\t";
-            detail = detail + product.getPrice()+ "\t\t";
-            detail = detail + product.getTotalCostProduct()+ "\n";
+            detail = detail + product.getQuantity() + "\t\t";
+            detail = detail + product.getName() + "\t\t";
+            detail = detail + product.getPrice() + "\t\t";
+            detail = detail + product.getTotalCostProduct() + "\n";
         }
         String footerLine = "-------------------------------\n";
         String totalCost = "Total\t\t\t\t\t\t"+getTotalCost();
 
         return header + headline + detail + footerLine + totalCost;
     }
-
+    /**
+     *
+     * @return String getDetailComplete
+     */
     public String getDetailComplete() {
         String header = "Cant\tProduct\t\tPrice\tTotal\n";
         String headline = "-------------------------------\n";
         String detail = "";
         for (Product product: products) {
-            detail = detail + product.getQuantity()+" ";
-            detail = detail + product.getUnit()+ "\t\t";
-            detail = detail + product.getName()+ "\t\t";
-            detail = detail + "$"+product.getPrice()+ "\t\t";
-            detail = detail + "$"+product.getTotalCostProduct()+ "\n";
+            detail = detail + product.getQuantity() +" ";
+            detail = detail + product.getUnit() + "\t\t";
+            detail = detail + product.getName() + "\t\t";
+            detail = detail + "$"+product.getPrice() + "\t\t";
+            detail = detail + "$"+product.getTotalCostProduct() + "\n";
         }
         String footerLine = "-------------------------------\n";
         String totalCost = "Total\t\t\t\t\t\t$"+getTotalCost();
@@ -55,19 +67,23 @@ public class Invoice {
         return header + headline + detail + footerLine + totalCost;
     }
 
+    /**
+     *
+     * @return String getDetailCompleteWithDiscount
+     */
     public String getDetailCompleteWithDiscount() {
         String header = "Cant\t\tProduct\t\tPrice\tCost\tDiscount Cost\n";
         String headline = "-----------------------------------------------------\n";
         String detail = "";
         for (Product product: products) {
             product.verifyPromotion();
-            detail = detail + product.getQuantity()+" ";
-            detail = detail + product.getUnit()+ "\t\t";
-            detail = detail + product.getName()+ "\t\t";
-            detail = detail + "$"+product.getPrice()+ "\t\t";
-            detail = detail + "$"+(product.getPrice() * product.getQuantity())+ "\t\t";
+            detail = detail + product.getQuantity() +" ";
+            detail = detail + product.getUnit() + "\t\t";
+            detail = detail + product.getName() + "\t\t";
+            detail = detail + "$"+product.getPrice() + "\t\t";
+            detail = detail + "$"+(product.getPrice() * product.getQuantity()) + "\t\t";
             if (product.isPromotion()) {
-                detail = detail + "$"+product.getTotalCostProduct()+ "\n";
+                detail = detail + "$"+product.getTotalCostProduct() + "\n";
             } else {
                 detail = detail + "-\n";
             }
