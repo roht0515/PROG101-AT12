075ae73f551f4f93dc3a883fcf377d79862c1414
maryangela23 <angelamdp16@gmail.com>
2020-08-06 16:15:14 -0400

    refactor FigureTest class

--- src/main/java/angela/practices/third/figure/FiguresTest.java
+++ src/main/java/angela/practices/third/figure/FiguresTest.java
@@ -2,12 +2,10 @@ package angela.practices.third.figure;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class FiguresTest {
-    private Scanner scanner;
     private int response = -1;
-    private GeometricFigure figure = null;
+    private Figure figure = null;
     private static final int OPTION_1 = 1;
     private static final int OPTION_2 = 2;
     private static final int OPTION_3 = 3;
@@ -16,7 +14,6 @@ public class FiguresTest {
     private BufferedReader reader;
 
     public FiguresTest() {
-        scanner = new Scanner(System.in);
     }
 
     public static void main(final String[] args) {
@@ -32,50 +29,37 @@ public class FiguresTest {
         reader = new BufferedReader(new InputStreamReader(System.in));
         do {
             response = prompt("select option:\n1.- Create rectangle\n2.- Create square\n3.- Display figure\n4.- Display details\n0.- exit\n");
-            if (response == OPTION_1) {
-                figure = Rectangle.readFigureData(reader);
-            }
-            if (response == OPTION_2) {
-                figure = Square.readFigureData(reader);
-            }
-            if (response == OPTION_3) {
-                if (figure != null) {
-                    figure.drawTxt();
-                } else {
-                    System.out.println("First create a figure\n");
-                }
-            }
-            if (response == OPTION_4) {
-                if (figure != null) {
-                    figure.printDescription();
-                } else {
-                    System.out.println("First create a figure\n");
-                }
+            switch (response) {
+                case OPTION_1:
+                    figure = Rectangle.readFigureData(reader);
+                case OPTION_2:
+                    figure = Square.readFigureData(reader);
+                case OPTION_3:
+                    if (figure != null) {
+                        figure.drawTxt();
+                    } else {
+                        System.out.println("First create a figure\n");
+                    }
+                case OPTION_4:
+                    if (figure != null) {
+                        figure.printDescription();
+                    } else {
+                        System.out.println("First create a figure\n");
+                    }
+                default:
+                    System.out.println("Enter a valid value");
             }
         } while (response != OPTION_0);
-        System.out.println(
-                "\nGoodbye");
+        System.out.println("\nGoodbye");
     }
     private int prompt(final String selection) {
-        String input;
-        int response = -1;
+        int input = -1;
         System.out.print(selection);
-        input = scanner.next();
-        if (input.equals("1")) {
-            response = OPTION_1;
+        try {
+            input = Integer.parseInt(reader.readLine());
+        } catch (Exception e) {
+            System.out.println("Invalid value: " + e.getMessage());
         }
-        if (input.equals("2")) {
-            response = OPTION_2;
-        }
-        if (input.equals("3")) {
-            response = OPTION_3;
-        }
-        if (input.equals("4")) {
-            response = OPTION_4;
-        }
-        if (input.equals("0")) {
-            response = OPTION_0;
-        }
-        return response;
+        return input;
     }
 }
