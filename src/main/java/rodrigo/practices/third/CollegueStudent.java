204114eeb16731a38e7bc6752a2a851f08397335
roht0515 <rodrigo051599@gmail.com>
2020-08-06 18:10:53 -0400

    adding again the classes

---
+++ src/main/java/rodrigo/practices/third/CollegueStudent.java
@@ -0,0 +1,22 @@
+package rodrigo.practices.third;
+public class CollegeStudent extends AbstracStudent {
+    private final int CANT_OF_STUDENTS = 3;
+    private final int APPROVAL_SCORE = 51;
+
+    CollegeStudent() { }
+    CollegeStudent(final String name) {
+    super.name = name;
+    }
+
+    public String computeCourseGrade() {
+        int totalScore = 0;
+        for (int i = 0; i < super.test.length; i++) {
+            totalScore += super.test[i];
+        }
+        totalScore /= CANT_OF_STUDENTS;
+        if (totalScore >= APPROVAL_SCORE) {
+            return super.courseGrade = "Aprobado";
+        }
+        return  super.courseGrade = "Reprobado";
+    }
+}
